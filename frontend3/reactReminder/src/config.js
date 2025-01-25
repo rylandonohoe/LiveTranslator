b17d8151575a5e9e666df7ccbeb91ae3d267a69d
Riana Dutta <rianadutta@Rianas-MacBook-Air.local>
2025-01-25 16:50:08 -0500

    3rd time lolz

---
+++ frontend3/reactReminder/src/config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyAiI5tNHddX3J_Wrgiezmwt7ZmbULCr5Do",
+  authDomain: "proj2-e96df.firebaseapp.com",
+  projectId: "proj2-e96df",
+  storageBucket: "proj2-e96df.firebasestorage.app",
+  messagingSenderId: "110750606722",
+  appId: "1:110750606722:web:ea4e480880394a0f971b9b",
+  measurementId: "G-VZXJ5WW60Q"
+};
+
+export default config
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+
+// Access Firebase services
+//const auth = getAuth(app);
+//const firestore = getFirestore(app);
+
+//export default { app, auth, firestore };
+//const app = initializeApp(firebaseConfig);
