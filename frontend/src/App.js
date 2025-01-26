ab0e943bb18eea8b01d83d98792d8b6f28f4d1d1
cmgvc <chloe.gavv@gmail.com>
2025-01-26 03:18:37 -0500

    frontend styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css'
 import VideoChatContainer from './VideoChatContainer'
+import Navbar from './components/Navbar'
 
 function App () {
   return (
     <div className='app'>
-      <h1 id='title'>Live translator</h1>
+      <Navbar/>
+      <div className='title-page'>
+        <h1 id='title'>Bread</h1>
+        <h2 id='subtitle'>Bidirectional Real-time Echo Audio Dialogue</h2>
+      </div>
       <VideoChatContainer/>
     </div>
   )
--- frontend/src/VideoChat.js
+++ frontend/src/VideoChat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import 'firebase/database'
 import classnames from 'classnames'
@@ -9,16 +9,23 @@ export default class VideoChat extends React.Component {
     this.state = {
       isLoggedIn: false,
       userToCall: "",   // Default to empty string, not null
-      username: ""      // Default to empty string, not null
+      username: "",     // Default to empty string, not null
+      errorMessage: null
     }
   }
 
   onLoginClicked = async () => {
+    if (!this.state.username) {
+      this.setState({ errorMessage: "Name cannot be empty." });
+      return;
+    }
     await this.props.onLogin(this.state.username)
     this.setState({
-      isLoggedIn: true
+      isLoggedIn: true,
+      errorMessage: null
     })
     document.querySelector('#title').style.display = 'none';
+    document.querySelector('#subtitle').style.display = 'none';
 
   }
 
@@ -29,12 +36,18 @@ export default class VideoChat extends React.Component {
   renderVideos = () => {
     return <>
     <div className={classnames('videos', { active: this.state.isLoggedIn })}>
-      <div>
-
+      <div className='my-video'>
         <video ref={this.props.setLocalVideoRef} autoPlay playsInline></video>
+        <div className='lang-toggle'>
+            <button onClick={this.props.toggleLang} className='toggle-btn'>
+              <img src="./assets/america.jpg" alt="English" class="flag" />
+            </button>
+            <button onClick={this.props.toggleLang} className='toggle-btn'></button>
+            <button onClick={this.props.toggleLang} className='toggle-btn'></button>
+        </div>
         <label id='username' className='username'>{this.state.username}</label>
       </div>
-      <div>
+      <div className='my-video'>
         <video ref={this.props.setRemoteVideoRef} autoPlay playsInline></video>
         <label id='username' className='username'>{this.props.connectedUser}</label>
       </div>
@@ -44,8 +57,7 @@ export default class VideoChat extends React.Component {
 
   renderForms = () => {
     return this.state.isLoggedIn
-      ? <div key='a' className='form'>
-        <label>Call to</label>
+      ? <div key='a' className='form2'>
         <input value={this.state.userToCall} type="text" onChange={e => this.setState({ userToCall: e.target.value })} />
         <button onClick={this.onStartCallClicked} id="call-btn" className="btn btn-primary">Call</button>
 
@@ -53,7 +65,9 @@ export default class VideoChat extends React.Component {
       : <div key='b' className='form'>
         <label>Please enter your name</label>
         <input value={this.state.username} type="text" onChange={e => this.setState({ username: e.target.value })} />
-
+        {this.state.errorMessage && (
+            <p style={{ color: '#bf0000', fontSize: '1rem' }}>{this.state.errorMessage}</p>
+          )}
         <button onClick={this.onLoginClicked} id="login-btn" className="btn btn-primary">Login</button>
 
       </div>
